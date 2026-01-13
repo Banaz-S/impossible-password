6f2addef593d7cee4fd352f2fe2142c440950b9b
Banaz Software <banazsleman2004@gmail.com>
2026-01-13 10:44:41 +0300

    Add StartScreen component with basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
+import StartScreen from "./components/StartScreen/StartScreen";
+
 function App() {
-  return (
-    <div
-      style={{
-        minHeight: "100vh",
-        backgroundColor: "#0f0f1a",
-        color: "white",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        fontSize: "32px",
-        fontFamily: "Arial, sans-serif",
-      }}
-    >
-      The Impossible Password
-    </div>
-  );
+  return <StartScreen />;
 }
 
 export default App;
---
+++ src/components/StartScreen/StartScreen.jsx
@@ -0,0 +1,14 @@
+import "./StartScreen.css";
+
+function StartScreen() {
+  return (
+    <div className="start-screen">
+      <h1 className="title">The Impossible Password</h1>
+      <p className="subtitle">Can you satisfy all the rules?</p>
+
+      <button className="start-button">Start Game</button>
+    </div>
+  );
+}
+
+export default StartScreen;
