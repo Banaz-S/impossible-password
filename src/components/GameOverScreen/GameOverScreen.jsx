87e78a136600c8806c6c71e528b51f496c736d53
Banaz Software <banazsleman2004@gmail.com>
2026-01-15 09:53:14 +0300

    adding ranking board to game over screen

--- src/components/GameOverScreen/GameOverScreen.jsx
+++ src/components/GameOverScreen/GameOverScreen.jsx
@@ -1,13 +1,13 @@
 import "./GameOverScreen.css";
 import RankingBoard from "../RankingBoard/RankingBoard";
 
-function GameOverScreen({ playerName, score, onRestart }) {
+function GameOverScreen({ playerName, score, onRestart, rankingVersion }) {
   return (
     <div className="game-over-screen">
       <h1>💀 GAME OVER 💀</h1>
       <p>{playerName}, time's up!</p>
       <p className="final-score">Score: {score}</p>
-
+      <RankingBoard refreshKey={rankingVersion} />
       <button onClick={onRestart}>Try Again</button>
     </div>
   );
